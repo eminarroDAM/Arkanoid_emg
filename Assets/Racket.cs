1bcc7db16de1d59731b61b10404ea4c57e176b67
ericmgeaDAM <41004702a@iespoblenou.org>
2022-03-08 17:46:35 +0100

    first commit

---
+++ Assets/Racket.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Racket : MonoBehaviour {
+	public float speed = 150.0f;	
+
+	//Utilitzarem FixedUpdate quan treballem en físiques
+    	void FixedUpdate() {
+        	float horizontalDirection = Input.GetAxisRaw("Horizontal");
+
+			GetComponent<Rigidbody2D>().velocity = Vector2.right * horizontalDirection * speed;
+    	}
+}
